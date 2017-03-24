e211e5c6586430098cd6e608f3ec81db79964950
Josh Shi <thehandsomepanther@gmail.com>
2017-03-24 16:00:36 -0400

    slightly more helpful error page

--- app.js
+++ app.js
@@ -6,7 +6,26 @@ var request = require('request');
 
 function handleError(err, req, res) {
   if (err.status == 404) {
-    res.render('pages/404')
+    var prismicApi
+
+    api(req, res).then(api => {
+      prismicApi = api
+      return prismicApi.getSingle('landing')
+    }).then(landing => {
+      prismicApi.query(
+        [
+          Prismic.Predicates.at("document.type", "thesis"),
+        ],
+        {
+          orderings: '[my.thesis.last_publication_date]',
+          pageSize: 5
+        }
+      ).then(function(response) {
+        res.render('error/404', {
+          pages: response.results
+        })
+      })
+    })
   } else {
     res.status(500).send('Error 500: ' + err.message);
   }
