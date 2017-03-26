de88e65699bae11a7ffca58786fd0144bee62ac4
Josh Shi <thehandsomepanther@gmail.com>
2017-03-25 22:51:35 -0500

    a slightly better way of doing a bad thing

--- util/injectCitations.js
+++ util/injectCitations.js
@@ -5,26 +5,10 @@ module.exports = function injectCitations(thesis) {
         if (v.data.copy !== undefined) {
           for (var copy of v.data.copy.value) {
             var re = /\[\d*\]/g
-            copy.spans = []
-            while ((match = re.exec(copy.text)) != null) {
-              copy.spans.push({
-                start: match.index,
-                end: match.index + match[0].length,
-                type: "hyperlink",
-                data: {
-                  type: "Link.web",
-                  value: {
-                    url: match[0]
-                  }
-                }
-              })
+            if (copy.spans == undefined) {
+              copy.spans = []
             }
-          }
-        }
-        if (v.fragments.copy !== undefined) {
-          for (var copy of v.fragments.copy.blocks) {
-            var re = /\[\d*\]/g
-            copy.spans = []
+
             while ((match = re.exec(copy.text)) != null) {
               copy.spans.push({
                 start: match.index,
