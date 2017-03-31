2b1d24ee1a4aab80202ab7fdffaab4f3300660ce
Josh Shi <thehandsomepanther@gmail.com>
2017-03-30 21:12:47 -0500

    add basic search

--- app.js
+++ app.js
@@ -148,3 +148,26 @@ app.get('/:uid', function(req, res) {
     }
   })
 })
+
+app.get('/search/:keyword', function(req, res) {
+  var prismicApi
+  var keyword = req.params.keyword
+
+  api(req, res).then(api => {
+    prismicApi = api
+    return prismicApi.getSingle('landing')
+  }).then(landing => {
+    if (landing) {
+      prismicApi.query(
+        [ Prismic.Predicates.fulltext("document", keyword) ]
+      ).then(function(posts) {
+        res.render('layouts/results', {
+          query: keyword,
+          results: posts.results
+        })
+      })
+    } else {
+      handleError(404, req, res)
+    }
+  })
+})
