173efc31b5324c4671040c21e8773a625924a7dd
Josh Shi <thehandsomepanther@gmail.com>
2017-04-21 16:56:58 -0400

    inject citations into quotes

--- util/injectCitations.js
+++ util/injectCitations.js
@@ -2,25 +2,29 @@ module.exports = function injectCitations(thesis) {
   for (var slice of thesis.getSliceZone('thesis.body').slices) {
     if (slice.sliceType == 'copy') {
       for (var v of slice.value.value) {
-        if (v.data.copy !== undefined) {
-          for (var copy of v.data.copy.value) {
-            var re = /\[\d*\]/g
-            if (copy.spans == undefined) {
-              copy.spans = []
-            }
+        var groups = ['copy', 'quote']
 
-            while ((match = re.exec(copy.text)) != null) {
-              copy.spans.push({
-                start: match.index,
-                end: match.index + match[0].length,
-                type: "hyperlink",
-                data: {
-                  type: "Link.web",
-                  value: {
-                    url: match[0]
+        for (var group of groups) {
+          if (v.data[group] !== undefined) {
+            for (var copy of v.data[group].value) {
+              var re = /\[\d*\]/g
+              if (copy.spans == undefined) {
+                copy.spans = []
+              }
+
+              while ((match = re.exec(copy.text)) != null) {
+                copy.spans.push({
+                  start: match.index,
+                  end: match.index + match[0].length,
+                  type: "hyperlink",
+                  data: {
+                    type: "Link.web",
+                    value: {
+                      url: match[0]
+                    }
                   }
-                }
-              })
+                })
+              }
             }
           }
         }
