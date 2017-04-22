27e81802bc2554c5f5bd8bd01e2bff738d75f38b
Josh Shi <thehandsomepanther@gmail.com>
2017-04-21 22:43:55 -0400

    fleshed out favicon

--- config.js
+++ config.js
@@ -16,7 +16,6 @@ module.exports = function() {
   app.set('port', process.env.PORT || 3000);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'pug');
-  app.use(favicon('public/assets/images/n.png'));
   app.use(logger('dev'));
   app.use(bodyParser());
   app.use(methodOverride());
