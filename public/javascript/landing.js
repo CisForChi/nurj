b3d6f0139c5e7600833d756440cca7b060ddabe9
Josh Shi <thehandsomepanther@gmail.com>
2017-04-28 12:05:24 -0500

    add circles to landing page

--- public/javascript/landing.js
+++ public/javascript/landing.js
@@ -2,14 +2,27 @@ invertHeaderOnCollision()
 $(window).scroll(invertHeaderOnCollision)
 $('.feature-title').hover(setActiveOnMouseEnter, removeActiveOnMouseLeave)
 
+$('.circle').click(function(e) {
+  animating = false
+  $('.feature-' + currentFeature).removeClass('is-shown')
+  $('.circle-' + currentFeature).removeClass('is-shown')
+  var i = parseInt($(this).attr('class').match(/circle-(\d*)/)[1])
+  currentFeature = i
+  $('.feature-' + currentFeature).addClass('is-shown')
+  $('.circle-' + currentFeature).addClass('is-shown')
+  setTimeout(function(){ animating = true }, 3000)
+})
+
 var numFeatures = $('.feature').length
 var currentFeature = 0
 var animating = true
 function advanceFeature() {
   if (animating) {
     $('.feature-' + currentFeature).removeClass('is-shown')
+    $('.circle-' + currentFeature).removeClass('is-shown')
     currentFeature = (currentFeature + 1) % numFeatures
     $('.feature-' + currentFeature).addClass('is-shown')
+    $('.circle-' + currentFeature).addClass('is-shown')
   }
 }
 
